cb1136c302696c28132d5b8da41e18610c74ee5b
Christian Rodemeyer <atombrenner@gmail.com>
2020-05-09 17:13:02 +0200

    adjust and test lenovo backup

--- lenovo/backup-lenovo.sh
+++ lenovo/backup-lenovo.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
 
 set -euo pipefail
+# shellcheck disable=SC2064
 trap "cd $(pwd)" ERR EXIT
 
-BACKUP="/home/christian/backups/lenovo/$(date +%Y-%m-%d-%H-%M)"
+(( EUID != 0 )) && echo "Please run as root" && exit
+
+# reading and using the disk id prevents running the script on the wrong computer
 DISK=$(readlink -f /dev/disk/by-id/nvme-SAMSUNG_MZVLW512HMJP-000L7_S359NX0J402722)
+BACKUPS_DIR="/home/christian/backups"
+BACKUP="${BACKUPS_DIR}/lenovo/$(date +%Y-%m-%d-%H-%M)"
 
 echo
-echo "Backup Lenovo $DISK to $BACKUP"
+echo "Backup Lenovo ${DISK} to ${BACKUP}"
 
 # Create backup folder
-mkdir -p "$BACKUP"
-cp "$0" "$BACKUP" # Backup this script
-cd "$BACKUP"
+mkdir -p "${BACKUP}"
+cp "$0" "${BACKUP}" # Backup this script
+cd "${BACKUP}"
 
 # Backup partition table
 sfdisk -d "${DISK}" >partition.dump
@@ -31,8 +36,8 @@ partclone.ext4 --clone -d -s "${DISK}p2" | zstd -9 -T0 > part2.zst
 # Backup Encrypted Partition 4
 cryptsetup luksDump "${DISK}p4" > luksDump4
 cryptsetup luksHeaderBackup "${DISK}p4" --header-backup-file luksHeader4
-cryptsetup --key-file - open "${DISK}p4" decrypted
-partclone.ext4 --clone -d -s /dev/mapper/decrypted | zstd -8 -T0 > part4.zst
+cryptsetup --key-file "${BACKUPS_DIR}/keyfile" open "${DISK}p4" decrypted
+partclone.ext4 --clone -d -s /dev/mapper/decrypted | zstd -9 -T0 > part4.zst
 cryptsetup close decrypted
 
 # Flush caches
